3aa86d12f95dd1bf93d28e1c9b2b061017235021
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-12-25 16:25:51 -0600

    Extension finished

--- contentScript.js
+++ contentScript.js
@@ -1,62 +1,72 @@
 (() => {
-	let youtubeLeftControls, youtubePlayer;
-	let currentVideo = "";
-	let currentVideoBookmarks = [];
+  let youtubeLeftControls, youtubePlayer;
+  let currentVideo = "";
+  let currentVideoBookmarks = [];
 
-	chrome.runtime.onMessage.addListener((obj, sender, response) => {
-		const { type, value, videoId } = obj;
+  const fetchBookmarks = () => {
+    return new Promise((resolve) => {
+      chrome.storage.sync.get([currentVideo], (obj) => {
+        resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+      });
+    });
+  };
 
-		if (type === "NEW") {
-			currentVideo = videoId;
-			newVideoLoaded();
-		}
-	});
+  const addNewBookmarkEventHandler = async () => {
+    const currentTime = youtubePlayer.currentTime;
+    const newBookmark = {
+      time: currentTime,
+      desc: "Bookmark at " + getTime(currentTime),
+    };
 
-	const fetchBookmarks = () => {
-		return new Promise((resolve) => {
-			chrome.storage.sync.get([ currentVideo ], obj => {
-				resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
-			});
-		});
-	}
+    currentVideoBookmarks = await fetchBookmarks();
 
-	const newVideoLoaded = async () => {
-		const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0]
-		currentVideoBookmarks = await fetchBookmarks();
+    chrome.storage.sync.set({
+      [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+    });
+  };
 
-		if (!bookmarkBtnExists) {
-			const bookmarkBtn = document.createElement("img");
+  const newVideoLoaded = async () => {
+    const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
 
-			bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-			bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-			bookmarkBtn.title = "Click to bookmark current timestamp";
+    currentVideoBookmarks = await fetchBookmarks();
 
-			youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-			youtubePlayer = document.getElementsByClassName("video-stream")[0];
+    if (!bookmarkBtnExists) {
+      const bookmarkBtn = document.createElement("img");
 
-			youtubeLeftControls.appendChild(bookmarkBtn);
-			bookmarkBtn.addEventListener('click', addNewBookmarkEventHandler);
-		}
-	}
+      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+      bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+      bookmarkBtn.title = "Click to bookmark current timestamp";
 
-	const addNewBookmarkEventHandler = async () => {
-		const currentTime = youtubePlayer.currentTime;
-		const newBookmar = {
-			time: currentTime,
-			desc: "Bookmark at " + getTime(currentTime),
-		};
+      youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+      youtubePlayer = document.getElementsByClassName('video-stream')[0];
 
-		currentVideoBookmarks = await fetchBookmarks();
+      youtubeLeftControls.appendChild(bookmarkBtn);
+      bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+    }
+  };
 
-		chrome.storage.sync.set({
-			[currentVideo]: JSON.stringify([ ...currentVideoBookmarks, newBookmar ].sort((a, b) => a.time - b.time))
-		});
-	}
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, value, videoId } = obj;
+
+    if (type === "NEW") {
+      currentVideo = videoId;
+      newVideoLoaded();
+    } else if (type === "PLAY") {
+      youtubePlayer.currentTime = value;
+    } else if ( type === "DELETE") {
+      currentVideoBookmarks = currentVideoBookmarks.filter((b) => b.time != value);
+      chrome.storage.sync.set({ [currentVideo]: JSON.stringify(currentVideoBookmarks) });
+
+      response(currentVideoBookmarks);
+    }
+  });
+
+  newVideoLoaded();
 })();
 
 const getTime = t => {
-	var date = new Date(0);
-	date.setSeconds(t);
+  var date = new Date(0);
+  date.setSeconds(t);
 
-	return date.toISOString().substring(11, 19);
-}
+  return date.toISOString().substr(11, 8);
+};
--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 export async function getActiveTabURL() {
-  let queryOptions = {
-    active: true,
-    currentWindow: true
-  };
-  let [ tab ] = await chrome.tabs.query(queryOptions);
-  return tab;
+  const tabs = await chrome.tabs.query({
+    currentWindow: true,
+    active: true
+  });
+
+  return tabs[0];
 }
